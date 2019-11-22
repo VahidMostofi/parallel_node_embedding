6f6e3d696e58292a23b0a51c28702b184e196dec
Vahid Mostofi <vahid@Vahids-MacBook-Pro.local>
2019-11-22 14:03:47 -0700

    remove go version of generate combinations, added the python version

--- core/generate_combinations.py
+++ core/generate_combinations.py
@@ -1,28 +1,35 @@
 import networkx as nx
-n = 8
-out = '['
-for i in range(n):
-    for j in range(i):
-        out += '({},{}),'.format(j,i)
-out = out[:-1]
-out += ']'
-print('maximum')
-print(out)
+import argparse
 
-print('minimum')
-combinations = [(0,1),(1,2),(2,3),(3,4),(4,5),(5,6),(6,7),(7,8),(8,9),(9,10),(10,11),(11,12),(12,13),(13,14),(14,15),
-(0,4),(4,8),(8,12),(12,15),(8,14),(4,13),(4,11),(4,15),(0,8),(3,10),(1,5),(2,8),(6,10)
-]
+def generate_combinations(n, click_size):
+    g = nx.Graph()
+    for i in range(click_size):
+        for j in range(i, click_size):
+            g.add_edge(i,j)
 
-g = nx.Graph()
-for e in combinations:
-    g.add_edge(*e)
+    single_count = int((n-click_size) / click_size)
+    for i in range(click_size, n):
+        connect_to = int((i-click_size) / single_count)
+        g.add_edge(connect_to, i)
+    combs = []
+    for e in g.edges():
+        e = (min(e[0],e[1]),max(e[0],e[1]))
+        combs.append(e)
+    return combs
 
-for gg in nx.all_pairs_bellman_ford_path(g):
-    start = gg[0]
-    for kk,vv in gg[1].items():
-        if len(vv) > 4:
-            print(start, kk, vv)
+def main(args):
+    combs = generate_combinations(args.n[0], args.click_size[0])
+    output = '['
+    for c in combs:
+        output += '({}, {}),'.format(c[0],c[1])
+    output = output[:-1]
+    output += ']'
+    print(output)
 
-print(nx.diameter(g))
-print(combinations)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--n', nargs=1, dest='n', type=int, required=True, help='number of batches')
+    parser.add_argument('--click', nargs=1, dest='click_size', type=int, required=True, help='size of central click')
+    args = parser.parse_args()
+
+    main(args)
