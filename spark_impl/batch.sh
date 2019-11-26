824b346c696fbe6de8560989947737325b6b65bf
Vahid Mostofi <vahidmostofi@outlook.com>
2019-11-26 15:37:39 -0700

    more args, more results

--- spark_impl/batch.sh
+++ spark_impl/batch.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 export PYSPARK_PYTHON=/home/vahid.mirzaebrahimmo/.conda/envs/pyspark2/bin/python
-spark-submit --executor-memory 22G --master spark://cn0588:7077 parallel_node_embedding_spark.py --batches 16 --dir ../data/livemocha --combinations max --wc 10 --wl 80 --dim 8
-spark-submit --executor-memory 22G --master spark://cn0588:7077 parallel_node_embedding_spark.py --batches 16 --dir ../data/livemocha --combinations max --wc 10 --wl 80 --dim 64
-spark-submit --executor-memory 22G --master spark://cn0588:7077 parallel_node_embedding_spark.py --batches 16 --dir ../data/livemocha --combinations max --wc 10 --wl 80 --dim 128
-spark-submit --executor-memory 22G --master spark://cn0588:7077 parallel_node_embedding_spark.py --batches 16 --dir ../data/livemocha --combinations max --wc 5 --wl 40 --dim 8
-spark-submit --executor-memory 22G --master spark://cn0588:7077 parallel_node_embedding_spark.py --batches 16 --dir ../data/livemocha --combinations max --wc 5 --wl 40 --dim 64
-spark-submit --executor-memory 22G --master spark://cn0588:7077 parallel_node_embedding_spark.py --batches 16 --dir ../data/livemocha --combinations max --wc 5 --wl 40 --dim 128
-spark-submit --executor-memory 22G --master spark://cn0588:7077 parallel_node_embedding_spark.py --batches 16 --dir ../data/livemocha --combinations max --wc 2 --wl 15 --dim 8
-spark-submit --executor-memory 22G --master spark://cn0588:7077 parallel_node_embedding_spark.py --batches 16 --dir ../data/livemocha --combinations max --wc 2 --wl 15 --dim 64
-spark-submit --executor-memory 22G --master spark://cn0588:7077 parallel_node_embedding_spark.py --batches 16 --dir ../data/livemocha --combinations max --wc 2 --wl 15 --dim 128
-spark-submit --executor-memory 22G --master spark://cn0588:7077 parallel_node_embedding_spark.py --batches 32 --dir ../data/livemocha --combinations max --wc 10 --wl 80 --dim 8
-spark-submit --executor-memory 22G --master spark://cn0588:7077 parallel_node_embedding_spark.py --batches 32 --dir ../data/livemocha --combinations max --wc 10 --wl 80 --dim 64
-spark-submit --executor-memory 22G --master spark://cn0588:7077 parallel_node_embedding_spark.py --batches 32 --dir ../data/livemocha --combinations max --wc 10 --wl 80 --dim 128
-spark-submit --executor-memory 22G --master spark://cn0588:7077 parallel_node_embedding_spark.py --batches 32 --dir ../data/livemocha --combinations max --wc 5 --wl 40 --dim 8
-spark-submit --executor-memory 22G --master spark://cn0588:7077 parallel_node_embedding_spark.py --batches 32 --dir ../data/livemocha --combinations max --wc 5 --wl 40 --dim 64
-spark-submit --executor-memory 22G --master spark://cn0588:7077 parallel_node_embedding_spark.py --batches 32 --dir ../data/livemocha --combinations max --wc 5 --wl 40 --dim 128
-spark-submit --executor-memory 22G --master spark://cn0588:7077 parallel_node_embedding_spark.py --batches 32 --dir ../data/livemocha --combinations max --wc 2 --wl 15 --dim 8
-spark-submit --executor-memory 22G --master spark://cn0588:7077 parallel_node_embedding_spark.py --batches 32 --dir ../data/livemocha --combinations max --wc 2 --wl 15 --dim 64
-spark-submit --executor-memory 22G --master spark://cn0588:7077 parallel_node_embedding_spark.py --batches 32 --dir ../data/livemocha --combinations max --wc 2 --wl 15 --dim 128
+# spark-submit --executor-memory 22G --master spark://cn0588:7077 parallel_node_embedding_spark.py --batches 32 --dir ../data/livemocha --combinations 2 --wc 5 --wl 40 --dim 64
+# spark-submit --executor-memory 22G --master spark://cn0588:7077 parallel_node_embedding_spark.py --batches 32 --dir ../data/livemocha --combinations 8 --wc 5 --wl 40 --dim 64
+
+# spark-submit --executor-memory 22G --master spark://cn0588:7077 parallel_node_embedding_spark.py --batches 32 --dir ../data/livemocha --combinations 16 --wc 5 --wl 40 --dim 64
+# spark-submit --executor-memory 22G --master spark://cn0588:7077 parallel_node_embedding_spark.py --batches 32 --dir ../data/livemocha --combinations 24 --wc 5 --wl 40 --dim 64
